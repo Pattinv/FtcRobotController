ce0fc079c2ed8c783d147dcc62c1ead99647cb3e
Team11574 <Pattinv@gmail.com>
2021-01-04 19:31:24 -0800

    Added a comment in Master

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GitHubWorkshop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GitHubWorkshop.java
@@ -2,5 +2,4 @@ package org.firstinspires.ftc.teamcode;
 // this file is super helpful
 public class GitHubWorkshop {
 }
-// this was created in NewBranch
 // changes to master here even more important
